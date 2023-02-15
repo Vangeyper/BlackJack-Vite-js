b757fe3c8e106d4e750b30c57ca6719be94f5b41
Vangeyper <vangeyperr@gmail.com>
2023-02-15 09:02:07 +0100

    añadido vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/BlackJack-Vite-js/',  // es el directorio o nombre que aparece en GITHUB para acceder a nuestra app (sin contar el dominio de GitHub que tengamos):
+    // https://vangeyper.github.io/BlackJack-Vite-js/
+});
